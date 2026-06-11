909d33f0834a3ace2a3c0c590e9338da62e4599a
Rubenp07 <rubenpana8@gmail.com>
2026-06-11 10:42:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -98,7 +98,8 @@ app.post('/update-keyinfo', async (req, res) => {
     if (!property) return res.status(400).json({ error: 'Falta propiedad' });
     const result = await sheets.spreadsheets.values.get({
       spreadsheetId: SHEET_ID,
-range: "\'Keys Information\'!G" + rowNum + ':O' + rowNum,    });
+      range: "'Keys Information'!F:F"
+    });
     const rows = result.data.values || [];
     let rowNum = null;
     for (let i = 0; i < rows.length; i++) {
